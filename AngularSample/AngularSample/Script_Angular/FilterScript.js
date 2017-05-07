ac526ee726ea1d19e7770088194a06f80b047f77
pintoo <pintoosintoo@gmail.com>
2017-05-07 22:07:37 +0530

    Filters
    Filters

---
+++ AngularSample/AngularSample/Script_Angular/FilterScript.js
@@ -0,0 +1,14 @@
+﻿/// referance path="../Script_Angular/angular.js"
+
+var myApp = angular.module("myApp", []).controller("myController", function ($scope) {
+
+    var employees = [
+        { Name: "Ben", DateOfBirth: new Date("November 23, 1980"), Gender: "Male", Salary: 5644449646.2659 },
+        { Name: "Sara", DateOfBirth: new Date("May 5, 1970"), Gender: "Female", Salary: 500000 },
+        { Name: "Mark", DateOfBirth: new Date("August 15, 1974"), Gender: "Male", Salary: 57000 },
+        { Name: "Pam", DateOfBirth: new Date("October 27, 1979"), Gender: "Female", Salary: 530000.59 },
+        { Name: "Todd", DateOfBirth: new Date("December 30, 1983"), Gender: "Male", Salary: 60000 }
+    ];
+    $scope.employees = employees;
+    $scope.rowLimit = 1;
+});
