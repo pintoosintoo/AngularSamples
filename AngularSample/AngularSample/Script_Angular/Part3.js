ecdb504d98936a683521e54c36698cfc5ebbc06d
pintoo <pintoosintoo@gmail.com>
2017-05-29 23:26:04 +0530

    Part3
    Part3

---
+++ AngularSample/AngularSample/Script_Angular/Part3.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+var myapp = angular.module("myapp", []).controller("ctrl", function ($scope) {
+    var Employee = {
+        firstName: "Saurabh",
+        lastName: "Kumar",
+        gender:"Male"
+
+    };
+    $scope.Employee = Employee;
+    $scope.message = "this is our 3rd application";
+});
