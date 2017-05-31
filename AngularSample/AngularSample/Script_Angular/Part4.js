8e966dafdc482eab04de926213fd639e70f63578
pintoo <pintoosintoo@gmail.com>
2017-05-31 23:26:50 +0530

    Part4 ng-src
    Part4 ng-src

---
+++ AngularSample/AngularSample/Script_Angular/Part4.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+
+var myapp = angular.module("myapp", []).controller("ctrl", function ($scope) {
+
+    country = {
+        CountryName: 'India',
+        State: 'UP',
+        ImageLiked: '/Images/Angular.png'
+    }
+    $scope.country = country;
+})
