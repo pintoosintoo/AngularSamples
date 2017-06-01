ce2aca010a38753e1eb42f2fc9a33f041e981617
pintoo <pintoosintoo@gmail.com>
2017-06-01 23:31:54 +0530

    Commit
    Commit

---
+++ AngularSample/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [EnableCors("*","*","*")]
+        [HttpOptions]
+        public string Post([FromBody]string value)
+        {
+            using (DB db=new WebApi.DB())
+            {
+                db.Students.Add(new WebApi.Student {Name=value,CreatedDate=DateTime.Now });
+                db.SaveChanges();
+                return "Success;";
+
+            }
+        }
+
+        [HttpPost]
+        public string Save([FromBody]string value)
+        {
+            using (DB db = new WebApi.DB())
+            {
+                db.Students.Add(new WebApi.Student { Name = value, CreatedDate = DateTime.Now });
+                db.SaveChanges();
+                return "Success;";
+
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
